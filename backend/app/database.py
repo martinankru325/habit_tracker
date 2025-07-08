dc1a35634a8ae10b261f5df3b84c87980858c736
Martin <martinankru927@gmail.com>
2025-07-08 08:50:16 +0300

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://martin:bn7XNpL2@db/habitdb")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://user:userpass@db/habitdb")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
